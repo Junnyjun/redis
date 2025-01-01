1d11429f380382e0fc40f6f035fb07d6fa0254df
Junnyjun <chbe5082@naver.com>
2025-01-01 22:40:15 +0900

    init

---
+++ src/main/kotlin/io/junnyland/redis/RedisApplication.kt
@@ -0,0 +1,11 @@
+package io.junnyland.redis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RedisApplication
+
+fun main(args: Array<String>) {
+    runApplication<RedisApplication>(*args)
+}
